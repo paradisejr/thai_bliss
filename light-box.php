63677163f286ea5ed9ef9a1164121b6a44584915
supakit.j <supakit.j@bizidea.co.th>
2014-12-08 22:55:47 +0700

    add light box products

---
+++ light-box.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="http://abstractcodify.com/thaibliss/dev_style.css">
+
+</head>
+<body>
+
+    <div class="container" >
+        <div id="treatments-details" class="treatments-details">
+            <div class="fancy-wrapper">
+                <h3>HONEY CENTELLA BODY POLISH</h3>
+
+                <div class="col-md-7 col-sm-7">
+                    <img src="http://abstractcodify.com/thaibliss/cache/uploads/Honey-Scrub286x168.jpg" alt="">
+                </div>
+                <div class="col-md-5 col-sm-5">
+
+                    <div class="content">
+                        <h4><a href="">HONEY CENTELLA BODY POLISH</a></h4>
+                        <p style="word-wrap: break-word;" >
+                            Refreshing&nbsp;Body&nbsp;Polish&nbsp;Scrub&nbsp;exfoliates&nbsp;the&nbsp;skin&nbsp;with&nbsp;blend&nbsp;of&nbsp;salts&nbsp;and&nbsp;nourishing&nbsp;honey&nbsp;and&nbsp;bran&nbsp;oils.&nbsp;It&nbsp;helps&nbsp;to&nbsp;rejuvenate&nbsp;and&nbsp;eliminate&nbsp;dead&nbsp;skin&nbsp;cells.&nbsp;Provides&nbsp;deep&nbsp;cleansing&nbsp;and&nbsp;polishing&nbsp;to&nbsp;leave&nbsp;skin&nbsp;feeling&nbsp;fresh,&nbsp;smooth&nbsp;and&nbsp;moisturised.<br />
+                            <br />
+                            <strong><span style="color:#fce082;">ADVICE</span></strong><br />
+                            <span style="color:#FFFFFF;"><span style="font-size:12px;"><span style="font-family: arial, sans-serif;">In&nbsp;bath&nbsp;or&nbsp;shower,&nbsp;massage&nbsp;a&nbsp;generous&nbsp;amount&nbsp;in&nbsp;circular&nbsp;movements&nbsp;onto&nbsp;the&nbsp;body,&nbsp;rinse&nbsp;well.&nbsp;Use&nbsp;2&nbsp;to&nbsp;3&nbsp;times&nbsp;a&nbsp;week.&nbsp;</span></span></span><br />
+                            <br />
+                            <span style="color:#fce082;"><strong>INGREDIENTS</strong></span><br />
+                            Glycerin,&nbsp;Sodium&nbsp;Chloride,&nbsp;PEG-40&nbsp;Hydrogenated&nbsp;Castor&nbsp;Oil,&nbsp;Beeswax,&nbsp;Caprylic/Capric&nbsp;Triglyceride,&nbsp;Cocamidopropyl&nbsp;Betaine,&nbsp;Oryza&nbsp;Sativa&nbsp;(Rice)&nbsp;Bran&nbsp;Oil,&nbsp;Oleth-5,&nbsp;Macadamia&nbsp;Ternifolia&nbsp;Seed&nbsp;Oil,&nbsp;Cetearyl&nbsp;Alcohol,&nbsp;Palmitic&nbsp;Acid,&nbsp;Stearic&nbsp;Acid,&nbsp;Sodium&nbsp;Coco-Sulfate,&nbsp;Citrus&nbsp;Aurantium&nbsp;Dulcis&nbsp;Oil,&nbsp;Sodium&nbsp;Lauroamphoacetate,&nbsp;Tocopheryl&nbsp;Acetate,&nbsp;Cocos&nbsp;Nucifera&nbsp;(Coconut)&nbsp;Oil,&nbsp;Aloe&nbsp;Barbadensis&nbsp;Leaf&nbsp;Extract,&nbsp;Mentha&nbsp;Piperita&nbsp;Leaf&nbsp;Oil,&nbsp;Honey&nbsp;Extract,&nbsp;Lavandula&nbsp;Angustifolia&nbsp;(Lavender)&nbsp;Oil,&nbsp;Cymbopogon&nbsp;Schoenanthus&nbsp;Oil,&nbsp;Citrus&nbsp;Aurantium&nbsp;Bergamia&nbsp;(Bergamot)&nbsp;Fruit&nbsp;Oil,&nbsp;Simmondsia&nbsp;Chinensis&nbsp;(Jojoba)&nbsp;Seed&nbsp;Oil,&nbsp;Aloe&nbsp;Barbadensis&nbsp;Leaf&nbsp;Juice&nbsp;Powder,&nbsp;Ascophyllum&nbsp;Nodosum&nbsp;Extract,&nbsp;Chlorphenesin,&nbsp;Iron&nbsp;Oxide&nbsp;(CI&nbsp;77492),&nbsp;Sodium&nbsp;Glycolate,&nbsp;Phenoxyethanol,&nbsp;Methylisothiazolinone,&nbsp;Iron&nbsp;Oxide(CI&nbsp;77499),&nbsp;Iron&nbsp;Oxide&nbsp;(CI&nbsp;77491),&nbsp;Centella&nbsp;Asiatica&nbsp;Extact.                </p>
+                        </div>
+
+                        <div class="col-sm-12 col-xs-12">
+                            <ul>
+                                <li class="price-details">
+                                    <span title="Size" class="time">Size</span>
+                                    <span class="price">150 ml - $32</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> <!-- end fancy-wrapper -->
+            </div> <!-- end maps details -->
+        </div>
+</body>
+</html>
---
+++ treatments-details.php
@@ -0,0 +1,51 @@
+
+
+<!-- Start FancyBox Maps -->
+<div id="treatments-details">
+	<div class="fancy-wrapper">
+		<h3>AROMATHERAPY OIL MASSAGES</h3>
+
+		<div class="col-md-7 col-sm-7">
+			<img src="img/item-products-1.jpg" alt="">
+		</div>
+		<div class="col-md-5 col-sm-5">
+
+			<div class="content">
+				<h4><a href="">AROMATHERAPY OIL MASSAGES</a></h4>
+				<p>
+					A deeply therapeutic treatment combines long
+					smooth strokes and gentle kneading to create
+					a feeling of physical relaxation and calm, whilst
+					strengthening the body’s vital energy so you
+					emerge with renewed spirit and cleared mind.
+					Truly a perfect way to end the day.
+				</p>
+			</div>
+			<div class="col-sm-12 col-xs-12">
+				<ul>
+					<li class="price-details">
+						<span class="time">30 minutes</span>
+						<span class="price">$55</span>
+					</li>
+					<li class="price-details">
+						<span class="time">1 hour</span>
+						<span class="price">$89</span>
+					</li>
+					<li class="price-details">
+						<span class="time">1 hour</span>
+						<span class="price">$89</span>
+					</li>
+					<li class="price-details">
+						<span class="time">1 hour 30 minutes</span>
+						<span class="price">$119</span>
+					</li>
+					<li class="price-details">
+						<span class="time">2 hour</span>
+						<span class="price">$159</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div> <!-- end fancy-wrapper -->
+</div> <!-- end maps details -->
+<!-- end FancyBox Maps -->
